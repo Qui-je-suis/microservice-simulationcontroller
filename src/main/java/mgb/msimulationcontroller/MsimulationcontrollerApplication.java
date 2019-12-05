6b54642894555648919c9ddd0095b621d77efe80
cedcass <cassignac.cedric@orange.fr>
2019-12-05 19:29:27 +0100

    first commit

---
+++ src/main/java/mgb/msimulationcontroller/MsimulationcontrollerApplication.java
@@ -0,0 +1,11 @@
+package mgb.msimulationcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsimulationcontrollerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsimulationcontrollerApplication.class, args);
+    }
+}
